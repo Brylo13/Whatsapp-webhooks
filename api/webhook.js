023f6716cb91560cfd25341df7a060fb454458db
Braulio <braulioaff@gmail.com>
2025-09-26 00:48:36 -0500

    Webhook completo

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
+  if (req.method === "GET") {
+    const VERIFY_TOKEN = "tu_token_secreto"; // pon aquí tu token
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode && token && mode === "subscribe" && token === VERIFY_TOKEN) {
+      return res.status(200).send(challenge);
+    } else {
+      return res.sendStatus(403);
+    }
+  }
+
   return res.status(200).json({ ok: true, method: req.method });
 }
